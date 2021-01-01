6cb512a674fc8b7910ac9930c05fc59d29e74132
Ngu0210 <dannynguyen312@gmail.com>
2021-01-02 01:53:07 +1100

    Completed register endpoint

--- controllers/auth_controller.py
+++ controllers/auth_controller.py
@@ -1,14 +1,26 @@
-from flask import Blueprint, about
+from flask import Blueprint, abort, jsonify, request
 from schemas.UserSchema import user_schema
-from models.User import Users
+from models.User import User
+from main import db
 
 auth = Blueprint("auth", __name__, url_prefix="/auth")
 
 @auth.route("/register", methods=["POST"])
 def auth_register():
-    user_fields = user.schema.load(request.json)
+
+    user_fields = user_schema.load(request.json)
     
     user = User.query.filter_by(email=user_fields["email"]).first()
 
     if user:
         return abort(400, description="Email already registered")
+
+    user = User()
+    user.email = user_fields["email"]
+    user.password = user_fields["password"]
+
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify(user_schema.dump(user))
